dbed51032813fafe4d0336846ccc39bc26c1697c
zulfuqar1 <zulfugarga@code.edu.az>
2023-06-18 19:50:53 +0400

    SearchUsersByName-method created

--- ADO.Net/ADO.Net/Program.cs
+++ ADO.Net/ADO.Net/Program.cs
@@ -82,3 +82,33 @@ void GetUsersCount()
         conn.Close();
     }
 }
+
+
+void SerachUsersByName(string search) 
+{
+    string query = $"SELECT * FROM Users WHERE Name like '%{search}%'";
+    using (SqlConnection conn = new SqlConnection(connectionStr))
+    {
+        try
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"id:{reader[0]}; Name:{reader[1]}; SurName:{reader[2]}; Phone:{reader[3]}; Adress:{reader[4]}; Email:{reader[5]}");
+                }
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    };
+    conn.Close();
+}
